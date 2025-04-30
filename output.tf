fccf1c73fc3033eb48c7de10493ef0a42e03ce95
Angad Wagh <waghangad@gmail.com>
2025-04-30 18:10:54 +0000

    commented output

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "created_s3_bucket_arn" {
    value       = module.create_s3_bucket.bucket_arn
 }
 
-output "eks_cluster_arn" {
-   description = "The ARN of the created eks."
-   value       = module.eks.cluster_arn
-}
+# output "eks_cluster_arn" {
+#   description = "The ARN of the created eks."
+#   value       = module.eks.cluster_arn
+#}

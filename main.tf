e80d0457a6bf2fd85ab80a4f84f841da529e00b7
Angad Wagh <waghangad@gmail.com>
2025-05-06 07:32:03 +0000

    comment eks

--- main.tf
+++ main.tf
@@ -22,45 +22,45 @@ module "create_s3_bucket" {
 # EKS Auto mode 
 # -------------
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.31"
 
-  cluster_name    = "study-eks-cluster"
-  cluster_version = "1.31"
+#   cluster_name    = "study-eks-cluster"
+#   cluster_version = "1.31"
 
-  # Optional
-  cluster_endpoint_public_access = false
+#   # Optional
+#   cluster_endpoint_public_access = false
 
-  cluster_addons = {
-  }
+#   cluster_addons = {
+#   }
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
+#   # Optional: Adds the current caller identity as an administrator via cluster access entry
+#   enable_cluster_creator_admin_permissions = true
 
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose", "system"]
-  }
+#   cluster_compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose", "system"]
+#   }
 
-  vpc_id     = "vpc-0f1ef7f44c5098ddb"
-  subnet_ids = ["subnet-00c853ba29eb48d3c", "subnet-0b12c0cac23004261"]
+#   vpc_id     = "vpc-0f1ef7f44c5098ddb"
+#   subnet_ids = ["subnet-00c853ba29eb48d3c", "subnet-0b12c0cac23004261"]
 
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      principal_arn = "arn:aws:iam::992293216837:user/Angad"
+#   access_entries = {
+#     # One access entry with a policy associated
+#     example = {
+#       principal_arn = "arn:aws:iam::992293216837:user/Angad"
 
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-          access_scope = {
-            type       = "cluster"
-          }
-        }
-      }
-    }
-  }
+#       policy_associations = {
+#         example = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+#           access_scope = {
+#             type       = "cluster"
+#           }
+#         }
+#       }
+#     }
+#   }
 
   tags = {
     Environment = "dev"
